97c7c9fd6a78587922a959f72aa3e1aa249b6df4
ramon trovão <ramonzinhg12@gmail.com>
2024-09-30 22:51:08 -0300

    chore: initial commit

---
+++ src/screens/LoginScreen/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+export function LoginScreen() {
+  return (
+    <S.LoginContainer>
+      <S.LoginTitle>Login Screen</S.LoginTitle>
+    </S.LoginContainer>
+  );
+}
---
+++ src/screens/LoginScreen/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const LoginContainer = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginTitle = styled.Text`
+  font-size: 24px;
+  font-weight: 700;
+  color: '#000';
+`;
